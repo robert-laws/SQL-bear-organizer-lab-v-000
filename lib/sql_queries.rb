bda3a2c5925193c7204a4166e055df66eecb5a64
Robert Laws <roblaws@me.com>
2017-10-07 11:48:15 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,11 +15,11 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT name, MIN(age) FROM bears"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT COUNT(*) FROM bears WHERE color = 'dark brown'"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
